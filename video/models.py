1db01524b64f0ad1083d6ee19d5c9b1104dce057
Prateek Varma <prtk.varma@gmail.com>
2021-01-05 22:23:49 -0600

    created video model and registered on admin

--- video/models.py
+++ video/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=30)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    video_file = models.FileField(upload_to='uploads')
+    thumbnail_file = models.FileField(upload_to='uploads')
+
+    def __str__(self):
+        return self.title
