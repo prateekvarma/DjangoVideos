d594ba5de0c84f31d1d4bb8d8dba88c9ec777803
Prateek Varma <prtk.varma@gmail.com>
2021-01-12 21:03:30 -0600

    changed individual video page's url to pass dynamic ID

--- video/views.py
+++ video/views.py
@@ -6,6 +6,6 @@ def index(request):
     return render(request, 'video/index.html')
 
 
-def video(request):
-    queried_video = Video.objects.get(pk=1)
+def video(request, video_id):
+    queried_video = Video.objects.get(pk=video_id)
     return render(request, 'video/video.html', {'queried_video': queried_video})
