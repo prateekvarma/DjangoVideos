7d28d64841e1daebd258bd86ac679ed5a75b05b1
Prateek Varma <prtk.varma@gmail.com>
2021-01-17 23:50:09 -0600

    view logic to display videos with pagination on video list page

--- video/views.py
+++ video/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . models import Video
 
 
 def index(request):
-    return render(request, 'video/index.html')
+    video_list = Video.objects.all()
+    videos = Paginator(video_list, 3) # 3 items on each page
+    grouped_videos = []
+    for page in videos.page_range: # page_rage gives total number of pages by paginator
+        # videos.page takes a param, and returns that page.
+        # object_list gives the list of all objects in that page
+        page_objects = videos.page(page).object_list
+        grouped_videos.append(page_objects)
+    return render(request, 'video/index.html', {'videos': videos})
 
 
 def video(request, video_id):
