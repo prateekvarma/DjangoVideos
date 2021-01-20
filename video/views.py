0c048e5f868d3b34022ee92bdcf663cdb9d9deb7
Prateek Varma <prtk.varma@gmail.com>
2021-01-19 20:50:04 -0600

    display all videos from DB to index page

--- video/views.py
+++ video/views.py
@@ -8,11 +8,11 @@ def index(request):
     videos = Paginator(video_list, 3) # 3 items on each page
     grouped_videos = []
     for page in videos.page_range: # page_rage gives total number of pages by paginator
-        # videos.page takes a param, and returns that page.
+        # videos.page below takes a param, and returns that page.
         # object_list gives the list of all objects in that page
         page_objects = videos.page(page).object_list
         grouped_videos.append(page_objects)
-    return render(request, 'video/index.html', {'videos': videos})
+    return render(request, 'video/index.html', {'videos': videos, 'grouped_videos': grouped_videos})
 
 
 def video(request, video_id):
